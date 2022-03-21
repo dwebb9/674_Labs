1cea6838b8ef2fa442bcd8fb7eeffaa6fe718655
Devon <devon.webb@gmail.com>
2022-03-21 12:22:44 -0600

    chap6 with 3 planes

--- chap6/mavsim_chap6.py
+++ chap6/mavsim_chap6.py
@@ -33,9 +33,18 @@ wind = WindSimulation(SIM.ts_simulation)
 mav = MavDynamics(SIM.ts_simulation)
 autopilot = Autopilot(SIM.ts_simulation)
 
+mav2 = MavDynamics(SIM.ts_simulation)
+autopilot2 = Autopilot(SIM.ts_simulation)
+
+mav3 = MavDynamics(SIM.ts_simulation)
+autopilot3 = Autopilot(SIM.ts_simulation)
+
 # autopilot commands
 from message_types.msg_autopilot import MsgAutopilot
 commands = MsgAutopilot()
+commands2 = MsgAutopilot()
+commands3 = MsgAutopilot()
+
 Va_command = Signals(dc_offset=25.0,
                      amplitude=3.0,
                      start_time=2.0,
@@ -49,6 +58,16 @@ course_command = Signals(dc_offset=np.radians(180),
                          start_time=5.0,
                          frequency=0.015)
 
+altitude_command2 = Signals(dc_offset=40.0,
+                           amplitude=50.0,
+                           start_time=0.0,
+                           frequency=0.02)
+                           
+altitude_command3 = Signals(dc_offset=140.0,
+                           amplitude=50.0,
+                           start_time=0.0,
+                           frequency=0.02)
+
 # initialize the simulation time
 sim_time = SIM.start_time
 
@@ -61,16 +80,28 @@ while sim_time < SIM.end_time:
     commands.course_command = course_command.square(sim_time)
     commands.altitude_command = altitude_command.trapezoid(sim_time)
 
+    commands2.airspeed_command = commands.airspeed_command
+    commands2.course_command = commands.course_command
+    commands2.altitude_command = altitude_command2.trapezoid(sim_time)
+
+    commands3.airspeed_command = commands.airspeed_command
+    commands3.course_command = commands.course_command
+    commands3.altitude_command = altitude_command3.trapezoid(sim_time)
+
     # -------autopilot-------------
     estimated_state = mav.true_state  # uses true states in the control
-    delta, commanded_state = autopilot.update(commands, estimated_state)
+    delta, commanded_state = autopilot.update(commands, mav.true_state)
+    delta2, commanded_state2 = autopilot2.update(commands2, mav2.true_state)
+    delta3, commanded_state3 = autopilot3.update(commands3, mav3.true_state)
 
     # -------physical system-------------
     current_wind = wind.update()  # get the new wind vector
     mav.update(delta, current_wind)  # propagate the MAV dynamics
-
+    mav2.update(delta2, current_wind)
+    mav3.update(delta3, current_wind)
+    
     # -------update viewer-------------
-    mav_view.update(mav.true_state)  # plot body of MAV
+    mav_view.update(mav.true_state, mav2.true_state, mav3.true_state)  # plot body of MAV
     data_view.update(mav.true_state,  # true states
                      estimated_state,  # estimated states
                      commanded_state,  # commanded states
